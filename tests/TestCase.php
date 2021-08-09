e5691bd4baefdeea0b981074c56d99bb7ab2faac
phamthao <phamthao03596@gmail.com>
2021-08-09 11:10:59 +0700

    Wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Vanthao03596\LaravelWalletEventSourcing\Tests;
 use Dyrynda\Database\LaravelEfficientUuidServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Spatie\EventSourcing\EventSourcingServiceProvider;
 use Vanthao03596\LaravelWalletEventSourcing\LaravelWalletEventSourcingServiceProvider;
@@ -16,6 +17,10 @@ class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
+
+        (new EventSourcingServiceProvider($this->app))->register();
+
+        $this->setUpDatabase();
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Vanthao03596\\LaravelWalletEventSourcing\\Database\\Factories\\'.class_basename($modelName).'Factory'
@@ -34,15 +39,6 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        include_once __DIR__.'/../database/migrations/create_snapshots_table.php.stub';
-        (new \CreateSnapshotsTable())->up();
-
-        include_once __DIR__.'/../database/migrations/create_stored_events_table.php.stub';
-        (new \CreateStoredEventsTable())->up();
-
-        include_once __DIR__.'/../database/migrations/create_wallet_event_sourcing_table.php.stub';
-        (new \CreateWalletEventSourcingTable())->up();
     }
 
     public function refreshServiceProvider()
@@ -50,5 +46,20 @@ class TestCase extends Orchestra
         app(LaravelWalletEventSourcingServiceProvider::class, ['app' => $this->app])
             ->register()
             ->boot();
+    }
+
+    protected function setUpDatabase()
+    {
+        Schema::dropIfExists('snapshots');
+        include_once __DIR__.'/../database/migrations/create_snapshots_table.php.stub';
+        (new \CreateSnapshotsTable())->up();
+
+        Schema::dropIfExists('stored_events');
+        include_once __DIR__.'/../database/migrations/create_stored_events_table.php.stub';
+        (new \CreateStoredEventsTable())->up();
+
+        (new \CreateWalletEventSourcingTable())->down();
+        include_once __DIR__.'/../database/migrations/create_wallet_event_sourcing_table.php.stub';
+        (new \CreateWalletEventSourcingTable())->up();
     }
 }
